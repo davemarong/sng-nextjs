e12652c2482a9461503161c3f423843312ac3c09
Dave Kjell Marong <davem@stacc.com>
2023-11-18 18:37:25 +0100

    Remove treeview line

--- src/pages/produkter/index.tsx
+++ src/pages/produkter/index.tsx
@@ -1,29 +1,63 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useRQResource from "@/utils/query/useRQResource";
 import Products from "@/components/products/Products";
-import TreeView from "@/components/treeView/TreeView";
 import Split1to3View from "@/components/layout/Split1to3View";
 import { Button, Typography } from "@mui/material";
+
 import {
+  QueryClient,
   keepPreviousData,
   useInfiniteQuery,
   useQuery,
+  useQueryClient,
 } from "@tanstack/react-query";
 import Buttons from "@/components/products/Buttons";
 import CircularProgress from "@mui/material/CircularProgress";
 import ListTreeView from "@/components/treeView/ListTreeView";
 import CustomTreeView from "@/components/treeView/CustomTreeView";
+import { Tree } from "react-arborist";
+import TreeViewComp from "@/components/treeView/TreeViewComp";
+import { request } from "@/utils/query/axios-extend";
 
 const Produkter = () => {
+  const queryClient = useQueryClient();
   const [page, setPage] = useState(1);
-  const [filters, setFilters] = useState<String[]>([]);
-  const { data: products, isLoading } = useRQResource(
-    ["products", page],
-    `http://localhost:1337/api/products?filters[id][$in][0]=3&filters[id][$in][1]=6&filters[id][$in][2]=8&pagination[page]=${page}&pagination[pageSize]=20`,
-    "Retrieve products",
-    { placeholderData: keepPreviousData }
-  );
-  console.log(products);
+  const [filters, setFilters] = useState<string[]>([]);
+  const [filterQuery, setFilterQuery] = useState("");
+  // const { data: products, isLoading } = useRQResource(
+  //   ["products"],
+  //   // ["products", page],
+  //   // `http://localhost:1337/api/products?pagination[page]=${page}&pagination[pageSize]=20${filterQuery}`,
+  //   `http://localhost:1337/api/products?`,
+  //   "Retrieve products"
+  //   // { placeholderData: keepPreviousData }
+  // );
+  const { data, isLoading, isError, refetch } = useQuery({
+    queryKey: ["products", "filter"],
+    queryFn: () =>
+      request({
+        url: "http://localhost:1337/api/products?" + filterQuery,
+        method: "get",
+      }),
+  });
+
+  useEffect(() => {
+    console.log(filters?.length);
+    console.log(filters);
+    if (filters && filters?.length > 0) {
+      const joinedFilterQuery = filters
+        .map((item, index) => {
+          return `&filters[Name][$contains][${index}]=${item}`;
+        })
+        .join("");
+      console.log(joinedFilterQuery);
+
+      setFilterQuery(joinedFilterQuery);
+    }
+  }, [filters]);
+  useEffect(() => {
+    refetch();
+  }, [filters]);
   if (isLoading) return <CircularProgress />;
 
   return (
@@ -32,11 +66,10 @@ const Produkter = () => {
         Pasta, ris, nudler og potetmos
       </Typography>
       <Buttons setPage={setPage} page={page} />
+
       <Split1to3View>
-        <CustomTreeView />
-        {/* <TreeView setFilters={setFilters} /> */}
-        {/* <ListTreeView /> */}
-        <Products products={products?.data?.data} />
+        <TreeViewComp setFilters={setFilters} filters={filters} />
+        <Products products={data?.data?.data} />
       </Split1to3View>
       <Buttons setPage={setPage} page={page} />
     </>
